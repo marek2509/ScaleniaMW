2fa9a9930472d52069d3cd0ce4c8b453a9d6c714
Marek <marek.wojciechowicz25@gmail.com>
2020-04-09 16:34:53 +0200

    dodanie opcji odczytywanie bazy firebird

--- ScaleniaMW/MainWindow.xaml.cs
+++ ScaleniaMW/MainWindow.xaml.cs
@@ -1,5 +1,10 @@
-﻿using System;
+﻿using FirebirdSql.Data.FirebirdClient;
+using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,8 +25,49 @@ namespace ScaleniaMW
     /// </summary>
     public partial class MainWindow : Window
     {
+        public static FbConnection connection = new FbConnection(@"User=SYSDBA;Password=wbg;Database= C:\Users\User\source\repos\ScaleniaMW\ScaleniaMW\bin\Debug\PLANTA I INNE.FDB;" + "DataSource=localhost; Port=3050;Dialect=3; Charset=NONE;Role=;Connection lifetime=15;Pooling=true;" +
+                                           "MinPoolSize=0;MaxPoolSize=50;Packet Size=8192;ServerType=0;");
+       
         public MainWindow()
         {
+          
+
+            DataTable dt = null;
+            connection.Open();
+            FbCommand command = new FbCommand();
+           
+            FbTransaction transaction = connection.BeginTransaction();
+            command.Connection = connection;
+            command.Transaction = transaction;
+            command.CommandText = "select obreby.id || '-' || dzialka.idd as NR_DZ, case WHEN JEDN_REJ.nkr is null then obreby.id * 1000 + JEDN_REJ.grp else JEDN_REJ.nkr end as NKR_Z_GRUPAMI from DZIALKA left outer join OBREBY on dzialka.idobr = OBREBY.id_id left outer join JEDN_REJ on dzialka.rjdr = JEDN_REJ.id_id order by NKR_Z_GRUPAMI";
+            
+            FbDataAdapter adapter = new FbDataAdapter(command);
+            dt = new DataTable();
+            //  adapter.Fill(dt);
+            adapter.Fill(dt);
+            foreach (var item in dt.Columns)
+            {
+
+                Console.Write(item + " ");
+            }
+           Console.WriteLine();
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                for (int j = 0; j < dt.Columns.Count; j++)
+                {
+                    Console.Write(dt.Rows[i][j] + " \t");
+                }
+                Console.WriteLine();
+
+
+                //Console.WriteLine(i+ " " + dt.Rows[i][0]); // "" + dt.Rows[i][1] + " " );
+
+            }
+
+
+
+            connection.Close();
+          
             InitializeComponent();
         }
 
@@ -44,16 +90,16 @@ namespace ScaleniaMW
 
         public static string[] pobranieWartoscZLinii(string LiniaTekstu)
         {
-
-
-            char[] charSeparators = new char[] { '\t' };
+            char[] charSeparators = new char[] { '\t', ' ' };
             string[] wartosciZlini = LiniaTekstu.Trim().Split(charSeparators);
-
             return wartosciZlini;
         }
 
+
+        List<Punkt> listaPunktów = new List<Punkt>();
         private void otworzEDZ(object sender, RoutedEventArgs e)
         {
+            listaPunktów = new List<Punkt>();
             Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
             dlg.DefaultExt = ".edz";
             dlg.Filter = "All files(*.*) | *.*|TXT Files (*.txt)|*.txt| CSV(*.csv)|*.csv| EDZ(*.edz)|*.edz";
@@ -73,26 +119,62 @@ namespace ScaleniaMW
                        }
                         else
                         {
-                            listaBezPustychLinii.Add(calyOdczzytanyTextLinie[i].Trim());
+                            listaBezPustychLinii.Add(calyOdczzytanyTextLinie[i].Trim().Replace(".", ","));
                         }
                     }
-                    Console.WriteLine("LISTALISTALISTALISTALISTALISTALISTALISTALISTA");
-                    foreach (var item in listaBezPustychLinii)
-                    {
-                        Console.WriteLine(item);
-                    }
 
+                    int przekierownik = 0;
                     for (int i = 0; i < listaBezPustychLinii.Count; i++)
                     {
                         string[] wartosciZlini = pobranieWartoscZLinii(listaBezPustychLinii[i]);
-                        Console.WriteLine(wartosciZlini.Length);
+                        List<string> listTmp = new List<string>();
+                        foreach (var item in wartosciZlini)
+                        {
+                            if (item.Trim().Equals("") || item.Trim().Equals(null))
+                            {
+                                continue;
+                            }
+                            else
+                            {
+                                listTmp.Add(item.Trim()); 
+                            }
+                        }
 
+                        if (przekierownik>8 && listTmp.Count.Equals(1))
+                        {
+                            przekierownik = 0; Console.WriteLine("ustaw 0");
+                        }
 
+                        przekierownik += listTmp.Count;
+                        if(przekierownik==1)
+                        {
+                            listaPunktów.Add(new Punkt() { NazwaDz = listTmp[0] });
+                        }else if(przekierownik == 7)
+                        {
+                            listaPunktów[listaPunktów.Count - 1].DzX1 = float.Parse(listTmp[0], CultureInfo.InvariantCulture); 
+                            listaPunktów[listaPunktów.Count - 1].DzY1= float.Parse(listTmp[1], CultureInfo.InvariantCulture); 
+                        }
+                        else if(przekierownik == 8)
+                        {
+                            listaPunktów[listaPunktów.Count - 1].ilePktow = int.Parse(listTmp[0], CultureInfo.InvariantCulture);
+                        }
+                        else if (przekierownik > 8)
+                        {
+                            listaPunktów[listaPunktów.Count - 1].listaWspPktu.Add(new Punkt.WspPktu() { NR = listTmp[0], X = float.Parse(listTmp[1], CultureInfo.InvariantCulture), Y = float.Parse(listTmp[2], CultureInfo.InvariantCulture) });
+                        }
 
                     }
 
 
-
+                    foreach (var item in listaPunktów)
+                    {
+                        Console.WriteLine(item.NazwaDz + " " + item.DzX1 + " " + item.DzY1 + " ile: " + item.ilePktow);
+                        foreach (var items in listaPunktów[listaPunktów.Count - 1].listaWspPktu)
+                        {
+                            Console.WriteLine( "NRP " + items.NR + " PX " + items.X + " PY " + items.Y);
+                        }
+                     
+                    }
 
                 }
                 catch (Exception esa)
@@ -106,15 +188,48 @@ namespace ScaleniaMW
 
                     Console.WriteLine(esa + "Błędny format importu działek");
                 }
-
-
-
-
-
-
             }
         }
 
+        private void ZapiszDoPliku(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog svd = new SaveFileDialog();
+            svd.DefaultExt = ".txt";
+            svd.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+            if (svd.ShowDialog() == true)
+            {
+                using (Stream s = File.Open(svd.FileName, FileMode.Create))
+                //  using (StreamWriter sw = new StreamWriter(s, Encoding.Default))
+                using (StreamWriter sw = new StreamWriter(s, Encoding.Default))
+                    try
+                    {
+                        try
+                        {
+                            StringBuilder sb = new StringBuilder();
+                            foreach (var item in listaPunktów)
+                            {
+                                sb.AppendLine(item.NazwaDz + "\t" + item.podajeKatUstawienia());
+                            }
 
+                            sw.Write(sb.ToString());
+                            sw.Close();
+                        }
+                        catch (Exception exc)
+                        {
+                            MessageBox.Show(exc.ToString() + "  problem z plikiem");
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        var resultat = MessageBox.Show(ex.ToString() + " Przerwać?", "ERROR", MessageBoxButton.YesNo);
+
+                        if (resultat == MessageBoxResult.Yes)
+                        {
+                            Application.Current.Shutdown();
+                        }
+                    }
+
+            }
+        }
     }
 }
--- ScaleniaMW/Punkt.cs
+++ ScaleniaMW/Punkt.cs
@@ -11,13 +11,130 @@ namespace ScaleniaMW
         public string NazwaDz;
         public float DzX1;
         public float DzY1;
-        int ilePktow;
+        public int ilePktow;
+
+
+        public List<WspPktu> listaWspPktu = new List<WspPktu>();
+
 
         public class WspPktu
         {
             public string NR;
-            public float X1;
-            public float Y1;
+            public float X;
+            public float Y;
+        }
+
+        float dlZeWsp(float x1, float y1, float x2, float y2)
+        {
+            double dlugoscMiedzPktami = Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+            return (float)dlugoscMiedzPktami;
+        }
+
+        public float podajeKatUstawienia()
+        {
+            float tmpDlugosc = 0;
+            float tmpAzymut = 0;
+            for (int i = 0; i < listaWspPktu.Count - 1; i++)
+            {
+                float oblDl = dlZeWsp(listaWspPktu[i].X, listaWspPktu[i].Y, listaWspPktu[i + 1].X, listaWspPktu[i + 1].Y);
+                float azymut = (float)oblAzymut(listaWspPktu[i].X, listaWspPktu[i].Y, listaWspPktu[i + 1].X, listaWspPktu[i + 1].Y);
+                if (tmpDlugosc < oblDl)
+                {
+                    tmpDlugosc = oblDl;
+                    tmpAzymut = azymut;
+                }
+            }
+
+            float katObrotu = tmpAzymut + 300;
+            while (katObrotu > 400)
+            {
+                katObrotu = katObrotu - 400;
+            }
+
+            if (katObrotu > 100 && katObrotu < 300)
+            {
+                katObrotu = katObrotu + 200;
+            }
+
+            while (katObrotu > 400)
+            {
+                katObrotu = katObrotu - 400;
+            }
+            return katObrotu;
+        }
+
+        double oblAzymut(float x1, float y1, float x2, float y2)
+        {
+            double tgCzwartaka = 0;
+            double czwartak = 0;
+
+            double deltaY = y2 - y1;
+            double deltaX = x2 - x1;
+
+            if (deltaX != 0)
+            {
+                tgCzwartaka = (deltaY / deltaX);
+            }
+            czwartak = (Math.Atan(tgCzwartaka));
+            czwartak = czwartak * 200 / Math.PI;
+            czwartak = Math.Abs(czwartak);
+            double azymut = 0;
+
+            if (deltaY > 0)
+            {
+                if (deltaX > 0)
+                {
+
+
+                    azymut = czwartak;
+
+                }
+                else if (deltaX < 0)
+                {
+
+                    azymut = 200 - czwartak;
+
+                }
+                else if (deltaX == 0)
+                {
+                    azymut = 100;
+                }
+
+            }
+            else if (deltaY < 0)
+            {
+                if (deltaX > 0)
+                {
+                    azymut = 400 - czwartak;
+                }
+                else if (deltaX < 0)
+                {
+                    azymut = 200 + czwartak;
+                }
+                else if (deltaX == 0)
+                {
+                    azymut = 300;
+                }
+            }
+            else if (deltaY == 0)
+            {
+
+
+                if (deltaX > 0)
+                {
+                    azymut = 0;
+                }
+                else if (deltaX < 0)
+                {
+                    azymut = 200;
+                }
+
+                else if (deltaX == 0)
+                {
+                    azymut = 0;
+                }
+            }
+            return azymut*180/200;
         }
     }
 }
