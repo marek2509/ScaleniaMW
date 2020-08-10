0cef4691226c8bd96e40919e4fd816440d4303a4
Marek Wojciechowicz <marek.wojciechowicz25@gmail.com>
2020-08-10 08:55:59 +0200

    publikacja nowej wersji 1.0.0.27

--- ScaleniaMW/ZsumwaneWartosciZPorownania.cs
+++ ScaleniaMW/ZsumwaneWartosciZPorownania.cs
@@ -15,27 +15,39 @@ namespace ScaleniaMW
             get => _wartPrzed;
             set
             {
-                _wartPrzed = Decimal.Round(value,2);
+                _wartPrzed = Decimal.Round(value, 2);
                 Roznice = WartPo - WartPrzed;
-                odch_3_Proc =Decimal.Round(WartPrzed * 0.03M,2);
+                Odch_3_Proc = Decimal.Round(WartPrzed * 0.03M, 2);
             }
         }
-       
         public decimal WartPo
         {
-            get => _wartPo;          
+            get => _wartPo;
             set
             {
-                _wartPo = Decimal.Round(value,2);
+                _wartPo = Decimal.Round(value, 2);
                 Roznice = WartPo - WartPrzed;
             }
         }
         public decimal Roznice { get; set; }
-        public decimal odch_3_Proc { get; set; }
+        public decimal Odch_3_Proc
+        {
+            get => _odch_3_proc;
+            set
+            {
+                _odch_3_proc = value;
+                if (Math.Abs(Roznice) > Odch_3_Proc)
+                {
+                    CzyDopOdch__3__proc = "NIE";
+                }
+            }
+        }
+        public string CzyDopOdch__3__proc { get; set; }
+        decimal _odch_3_proc;
         decimal _wartPo;
         decimal _wartPrzed;
         public int IdPo;
-        
+
         public ZsumwaneWartosciZPorownania()
         {
         }
@@ -51,6 +63,6 @@ namespace ScaleniaMW
         public void wypiszWConsoli()
         {
             Console.WriteLine(NKR + "<NKR WartPrzed>" + WartPrzed + " " + WartPo + "<wart po ROZNICE>" + Roznice + " idPo:>" + IdPo + "nkrprzed>" + Nkr_Przed);
-        }      
+        }
     }
 }
