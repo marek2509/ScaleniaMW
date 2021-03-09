cbbb0151451edb1074cf60d8ad1cf38eafb57ef2
Marek Wojciechowicz <marek.wojciechowicz25@gmail.com>
2021-03-09 15:30:02 +0100

    praca na wykazem wydzielonych ekwiwalentów

---
+++ ScaleniaMW/DbWykazEkwiw.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScaleniaMW
+{
+    public class Obreb
+    {
+        public int IdObrebu { get; set; }
+        public int NrObrebu { get; set; }
+        public string Nazwa { get; set; }
+
+        public Obreb(int id, int nr, string nazwa)
+        {
+            IdObrebu = id;
+            NrObrebu = nr;
+            Nazwa = nazwa;
+        }
+
+    }
+
+    public static class ListaObrebow
+    {
+        public static List<Obreb> Obreby = new List<Obreb>();
+        public static void DodajObreb(int id, int nr, string nazwa)
+        {
+            Obreby.Add(new Obreb(id, nr, nazwa));
+        }
+        public static void ClearData()
+        {
+            Obreby = new List<Obreb>();
+        }
+    }
+
+    public class JR_Nowa
+    {
+        public int IdJednRejN { get; set; }
+        public int Ijr { get; set; }
+        public int Nkr { get; set; }
+        public bool Odcht { get; set; }
+        public bool Zgoda { get; set; }
+        public string Uwaga { get; set; }
+        public string NazwaObrebu
+        {
+            get => ListaObrebow.Obreby.Exists(x => x.IdObrebu == _id_obr) ? ListaObrebow.Obreby.Find(x => x.IdObrebu == _id_obr).Nazwa : "BRAK_OBREBU";
+
+            private set
+            {
+            }
+        }
+        public int NrObr
+        {
+            get => ListaObrebow.Obreby.Exists(x => x.IdObrebu == _id_obr) ? ListaObrebow.Obreby.Find(x => x.IdObrebu == _id_obr).NrObrebu : 0;
+
+            private set
+            {
+            }
+        }
+
+        public List<Wlasciciel> Wlasciciele = new List<Wlasciciel>();
+
+        public int _id_obr;
+
+        public JR_Nowa(int idJednRejN, int ijr, int nkr, bool odcht, bool zgoda, string uwaga, int id_obr)
+        {
+            IdJednRejN = idJednRejN;
+            Ijr = ijr;
+            Nkr = nkr;
+            Odcht = odcht;
+            Zgoda = zgoda;
+            Uwaga = uwaga;
+            _id_obr = id_obr;
+        }
+
+        public void DodajWlasciciela(Wlasciciel wlasciciel)
+        {
+            if (wlasciciel.IdMalzenstwa > 0)
+            {
+                Wlasciciele.Add(new Wlasciciel(wlasciciel.Udzial, wlasciciel.Udzial_NR, wlasciciel.NazwaWlasciciela.Remove(wlasciciel.NazwaWlasciciela.IndexOf("Ż:")).Replace("M:", ""), wlasciciel.Adres.Remove(wlasciciel.Adres.IndexOf("Ż:")).Replace("M:", ""), wlasciciel.IdMalzenstwa));
+                Wlasciciele.Add(new Wlasciciel(wlasciciel.Udzial, wlasciciel.Udzial_NR, wlasciciel.NazwaWlasciciela.Remove(1, wlasciciel.NazwaWlasciciela.IndexOf("Ż:")).Replace("M:", ""), wlasciciel.Adres.Remove(1, wlasciciel.Adres.IndexOf("Ż:")).Replace("M:", ""), wlasciciel.IdMalzenstwa));
+            }
+            else
+            {
+                Wlasciciele.Add(wlasciciel);
+            }
+
+        }
+
+        public void Wypisz()
+        {
+            Console.WriteLine(IdJednRejN + " " + Ijr + " " + Nkr + " " + Odcht + " " + Zgoda + " " + Uwaga + " OBR:" + NazwaObrebu);
+        }
+    }
+
+
+
+    public static class JednostkiRejestroweNowe
+    {
+        public static List<JR_Nowa> Jedn_REJ_N = new List<JR_Nowa>();
+        public static void DodajJrNowa(int idJednRejN, int ijr, int nkr, bool odcht, bool zgoda, string uwaga, int id_obr)
+        {
+            Jedn_REJ_N.Add(new JR_Nowa(idJednRejN, ijr, nkr, odcht, zgoda, uwaga, id_obr));
+        }
+
+        public static void ClearData()
+        {
+            Jedn_REJ_N = new List<JR_Nowa>();
+        }
+
+        public static void Wypisz()
+        {
+            foreach (var item in Jedn_REJ_N)
+            {
+                item.Wypisz();
+            }
+        }
+    }
+
+    public class Wlasciciel
+    {
+        public Wlasciciel( string udzial, double udzial_NR, string nazwaWlasciciela, string adres, int idMalzenstwa)
+        {
+            Udzial = udzial;
+            Udzial_NR = udzial_NR;
+            NazwaWlasciciela = nazwaWlasciciela;
+            Adres = adres;
+            IdMalzenstwa = idMalzenstwa;
+        }
+
+        public string Udzial { get; set; }
+        public double Udzial_NR { get; set; }
+        public string NazwaWlasciciela { get; set; }
+        public string Adres { get; set; }
+        public int IdMalzenstwa { get; set; }
+
+        public void WypiszWKoncoli()
+        {
+            Console.WriteLine(Udzial + " " + NazwaWlasciciela + " " + Adres);
+        }
+
+    }
+
+}
--- ScaleniaMW/WindowEdycjaDokumentow.xaml.cs
+++ ScaleniaMW/WindowEdycjaDokumentow.xaml.cs
@@ -1,18 +1,13 @@
-﻿using Microsoft.Win32;
+﻿using FirebirdSql.Data.FirebirdClient;
+using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.IO;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace ScaleniaMW
 {
@@ -21,35 +16,10 @@ namespace ScaleniaMW
     /// </summary>
     public partial class WindowEdycjaDokumentow : Window
     {
-        /* public void odczytUstawien()
-         {
-             try
-             {
-                 checkBoxPodzialSekcji.IsChecked = Properties.Settings.Default.CheckBoxPodzialSekcji;
-             }
-             catch
-             {
-                 Console.WriteLine("Nie odczytano ustawień");
-             }
-         }
-
-         public void zapisUstawien()
-         {
-             try
-             {
-                 Properties.Settings.Default.CheckBoxPodzialSekcji = (bool)checkBoxPodzialSekcji.IsChecked;
-             }
-             catch
-             {
-                 Console.WriteLine("Nie zapisno ustawień");
-             }
-         }*/
-
-
-
         public WindowEdycjaDokumentow()
         {
             InitializeComponent();
+            textBlockSciezka.Text = Properties.Settings.Default.PathFDB;
             // odczytUstawien();
         }
 
@@ -73,7 +43,6 @@ namespace ScaleniaMW
             try
             {
                 all = System.IO.File.ReadAllLines(a, Encoding.Default);
-
             }
             catch (Exception e)
             {
@@ -155,9 +124,6 @@ namespace ScaleniaMW
             }
         }
 
-
-
-
         bool SprawdzCzySzacunekTRUECzyKlasyfikacjaFALSE(string liniaZTektowki)
         {
             liniaZTektowki = liniaZTektowki.Trim();
@@ -189,7 +155,6 @@ namespace ScaleniaMW
             }
             return true;
         }
-
 
         string UsunWartZSzacunku(string liniaZTektowki, ref StringBuilder sbPuste)
         {
@@ -246,57 +211,8 @@ namespace ScaleniaMW
                     }
                 }
             }
-            /*  while (true)
-              {
-                  int indexOstatniMyslnik = liniaZTektowki.LastIndexOf('-');
-                  int indexOstatniUkosnik = liniaZTektowki.LastIndexOf('/');
-                  if (indexOstatniMyslnik > indexOstatniUkosnik)
-                  {
-                      liniaZTektowki = liniaZTektowki.Remove(indexOstatniMyslnik, 1);
-                      liniaZTektowki = liniaZTektowki.Insert(indexOstatniMyslnik, "/");
-                  }
-                  else
-                  {
-                      break;
-                  }
-              }*/
-
-
-
             return liniaZTektowki;
-
         }
-
-
-
-        // 6-106                    6-38/Lzr                           506
-        // 4-1/1                    4-898/Lzr-ŁVI-11478                  90.47
-        /*  string UsunWartZKlasyfkacji(string liniaZTektowki, ref StringBuilder sbPuste)
-          {
-
-              if (liniaZTektowki.Contains("pusty") || liniaZTektowki.Trim() == "")
-              {
-                  sbPuste.AppendLine(liniaZTektowki);
-                  return (liniaZTektowki);
-              }
-
-              int ukosnikKonturu = liniaZTektowki.IndexOf('/', liniaZTektowki.IndexOf(' '));
-              int ostatniMyslnik = liniaZTektowki.LastIndexOf('-');
-              if (ostatniMyslnik > ukosnikKonturu)
-              {
-
-
-
-              }
-
-              liniaZTektowki.Replace('-', '/');
-              int ileZnakowUsunac = liniaZTektowki.IndexOf(' ', ostatniMyslnik) - ostatniMyslnik;
-              //   Console.WriteLine( calyOdczzytanyTextLinie[i] +  " last index -: " + ostatniMyslnik + " Dł: " + dlTekstu + " wynik:" + calyOdczzytanyTextLinie[i].Remove(ostatniMyslnik, ileZnakowUsunac));
-
-              //   liniaZTektowki = liniaZTektowki.Replace(ostatniMyslnik, ileZnakowUsunac);
-
-              return liniaZTektowki;
-          }*/
 
 
         void zapisDoPliku(string tekstDoZapisu, string format = ".rtf")
@@ -344,5 +260,178 @@ namespace ScaleniaMW
         {
             zapisDoPliku(Properties.Resources.WzorzecRejestrPoScaleniu.ToString());
         }
+
+        public void ustawProperties(string FileName)
+        {
+            Properties.Settings.Default.PathFDB = FileName;
+            textBlockSciezka.Text = Properties.Settings.Default.PathFDB;
+            Properties.Settings.Default.Save();
+        }
+
+        private void ustawSciezkeFDB(object sender, RoutedEventArgs e)
+        {
+
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            if (!(Properties.Settings.Default.PathFDB.Equals("") || Properties.Settings.Default.PathFDB.Equals(null)))
+            {
+                dlg.InitialDirectory = Properties.Settings.Default.PathFDB.ToString().Substring(0, Properties.Settings.Default.PathFDB.LastIndexOf("\\"));
+            }
+
+            //dlg.InitialDirectory = @"C:\";
+            dlg.DefaultExt = ".edz";
+            dlg.Filter = "All files(*.*) | *.*|TXT Files (*.txt)|*.txt| CSV(*.csv)|*.csv| EDZ(*.edz)|*.edz";
+
+            Nullable<bool> result = dlg.ShowDialog();
+            if (result == true)
+            {
+                try
+                {
+                    ustawProperties(dlg.FileName);
+                    itemPolaczZBaza.Background = Brushes.Transparent;
+                }
+                catch (Exception esa)
+                {
+                    var resultat = MessageBox.Show(esa.ToString() + " Przerwać?", "ERROR", MessageBoxButton.YesNo);
+                    if (resultat == MessageBoxResult.Yes)
+                    {
+                        Application.Current.Shutdown();
+                    }
+                    Console.WriteLine(esa + "Błędny format importu działek");
+                }
+            }
+        }
+        public static string connectionString;
+        public static void aktualizujSciezkeZPropertis()
+        {
+            connectionString = @"User=" + Properties.Settings.Default.Login + ";Password=" + Properties.Settings.Default.Haslo + ";Database= " + Properties.Settings.Default.PathFDB + "; DataSource=localhost; Port=3050;Dialect=3; Charset=NONE;Role=;Connection lifetime=15;Pooling=true;" +
+                                  "MinPoolSize=0;MaxPoolSize=50;Packet Size=8192;ServerType=0;";
+        }
+
+        public DataTable odczytajZSql(string zapytanieSQL)
+        {
+            try
+            {
+                aktualizujSciezkeZPropertis();
+                using (var connection = new FbConnection(connectionString))
+                {
+                    connection.Open();
+                    FbCommand command = new FbCommand();
+                    FbTransaction transaction = connection.BeginTransaction();
+                    command.Connection = connection;
+                    command.Transaction = transaction;
+                    command.CommandText = zapytanieSQL;
+                    FbDataAdapter adapter = new FbDataAdapter(command);
+                    DataTable dt = new DataTable();
+                    adapter.Fill(dt);
+                    connection.Close();
+                    return dt;
+                }
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+
+
+        private void PolaczZBaza(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                itemPolaczZBaza.Background = Brushes.SeaGreen;
+                itemPolaczZBaza.Header = "Połączono z " + Properties.Settings.Default.PathFDB.Substring(Properties.Settings.Default.PathFDB.LastIndexOf('\\') + 1);
+
+
+                DataTable obreby = odczytajZSql(Constants.SQL_Obreby);
+                for (int i = 0; i < obreby.Rows.Count; i++)
+                {
+                    ListaObrebow.DodajObreb(Convert.ToInt32(obreby.Rows[i][0]), Convert.ToInt32(obreby.Rows[i][1]), obreby.Rows[i][2].ToString());
+                }
+
+                //jn.id_id, jn.ijr, jn.nkr, jn.ODCHT, jn.zgoda, jn.uwg, jn.id_obr
+                DataTable jednostkaRejestrowa = odczytajZSql(Constants.SQLJedn_rej_N);
+                for (int i = 0; i < jednostkaRejestrowa.Rows.Count; i++)
+                {
+                    int idJednRejN = jednostkaRejestrowa.Rows[i][0].Equals(DBNull.Value) ? 0 : Convert.ToInt32(jednostkaRejestrowa.Rows[i][0]);
+                    int ijr = jednostkaRejestrowa.Rows[i][1].Equals(DBNull.Value) ? 0 : Convert.ToInt32(jednostkaRejestrowa.Rows[i][1]);
+                    int nkr = jednostkaRejestrowa.Rows[i][2].Equals(DBNull.Value) ? 0 : Convert.ToInt32(jednostkaRejestrowa.Rows[i][2]);
+                    bool odcht = jednostkaRejestrowa.Rows[i][3].Equals(DBNull.Value) ? false : Convert.ToBoolean(jednostkaRejestrowa.Rows[i][3]);
+                    bool zgoda = jednostkaRejestrowa.Rows[i][4].Equals(DBNull.Value) ? false : Convert.ToBoolean(jednostkaRejestrowa.Rows[i][4]);
+                    string uwaga = jednostkaRejestrowa.Rows[i][5].ToString().Equals(DBNull.Value) ? "" : jednostkaRejestrowa.Rows[i][5].ToString();
+                    int idobr = jednostkaRejestrowa.Rows[i][6].Equals(DBNull.Value) ? 0 : Convert.ToInt32(jednostkaRejestrowa.Rows[i][6]);
+                    JednostkiRejestroweNowe.DodajJrNowa(idJednRejN, ijr, nkr, odcht, zgoda, uwaga, idobr);    // jednostkaRejestrowa.Rows[i][0]);
+                }
+
+
+             //   dgMy.ItemsSource = JednostkiRejestroweNowe.Jedn_REJ_N;
+
+                DataTable wlasciciele = odczytajZSql(Constants.SQLWlascicieleAdresyUdziayIdNKRNOWY);
+                for (int i = 0; i < wlasciciele.Rows.Count; i++)
+                {
+                    int idJednN = wlasciciele.Rows[i][0].Equals(DBNull.Value) ? 0 : Convert.ToInt32(wlasciciele.Rows[i][0]);
+                    string udzial = wlasciciele.Rows[i][1].ToString().Equals(DBNull.Value) ? "" : wlasciciele.Rows[i][1].ToString();
+                    double udzial_NR = wlasciciele.Rows[i][2].Equals(DBNull.Value) ? 0 : Convert.ToDouble(wlasciciele.Rows[i][2]);       
+                    string nazwaWlasciciela = wlasciciele.Rows[i][3].ToString().Equals(DBNull.Value) ? "" : wlasciciele.Rows[i][3].ToString();
+                    string adres = wlasciciele.Rows[i][4].ToString().Equals(DBNull.Value) ? "" : wlasciciele.Rows[i][4].ToString();
+                    int idMalzenstwa = wlasciciele.Rows[i][5].Equals(DBNull.Value) ? 0 : Convert.ToInt32(wlasciciele.Rows[i][5]);
+
+                    
+                    Wlasciciel wlasciciel = new Wlasciciel(udzial, udzial_NR, nazwaWlasciciela.ToUpper(), adres.ToUpper(), idMalzenstwa);
+                    JednostkiRejestroweNowe.Jedn_REJ_N.Find(x => x.IdJednRejN == idJednN).DodajWlasciciela(wlasciciel);
+                }
+
+                Console.WriteLine("heja");
+                JednostkiRejestroweNowe.Jedn_REJ_N.Find(x => x.Ijr == 4103).Wlasciciele.ForEach(x => x.WypiszWKoncoli());
+                Console.WriteLine("endo:");
+
+            }
+            catch (Exception ex)
+            {
+
+                itemPolaczZBaza.Background = Brushes.Red;
+                itemPolaczZBaza.Header = "Połącz z bazą";
+                textBlockLogInfo.Text = "Problem z połączeniem z bazą FDB " + ex.Message;
+                przejdzDoOknaLogowania(ex.Message);
+            }
+        }
+
+        void przejdzDoOknaLogowania(string s)
+        {
+            if (s.Contains("password"))
+            {
+                przejdzDoUstawLoginIHaslo();
+            }
+        }
+
+        void przejdzDoUstawLoginIHaslo()
+        {
+            textBoxLogin.Text = Properties.Settings.Default.Login;
+            textBoxHaslo.Password = Properties.Settings.Default.Haslo;
+            panelLogowania.Visibility = Visibility.Visible;
+        }
+
+        private void UstawLoginIHaslo(object sender, RoutedEventArgs e)
+        {
+            przejdzDoUstawLoginIHaslo();
+        }
+
+        private void ButtonZapiszLogIHaslo(object sender, RoutedEventArgs e)
+        {
+            Properties.Settings.Default.Login = textBoxLogin.Text;
+            Properties.Settings.Default.Haslo = textBoxHaslo.Password;
+            Properties.Settings.Default.Save();
+            panelLogowania.Visibility = Visibility.Hidden;
+        }
+
+        private void Button_Anuluj(object sender, RoutedEventArgs e)
+        {
+            panelLogowania.Visibility = Visibility.Hidden;
+        }
+
+
+
+
+
     }
 }
