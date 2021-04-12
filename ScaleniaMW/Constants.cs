d090f5656a156dc8d5bc2370b4b10233c764a790
Marek Wojciechowicz <marek.wojciechowicz25@gmail.com>
2021-04-12 12:23:07 +0200

    testowe zatwierdzenie

--- ScaleniaMW/Constants.cs
+++ ScaleniaMW/Constants.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ScaleniaMW
 {
-    static class Constants
+    static class Constants //testowy komentarz
     {
        /* public const string podzialSekcjiNaStronieNieparzystej0 = @" \sect }\sectd \ltrsect\sbkodd\linex0\headery708\footery708\colsx708\endnhere\sectdefaultcl\sectrsid13334316\sftnbj \pard\plain \ltrpar\ql \li0\ri0\sa200\sl276\slmult1\widctlpar\wrapdefault\aspalpha\aspnum\faauto\adjustright\rin0\lin0\itap0\pararsid16596786 \rtlch\fcs1 \af0\afs22\alang1025 \ltrch\fcs0 \fs22\lang1045\langfe1045\loch\af31506\hich\af31506\dbch\af31505\cgrid\langnp1045\langfenp1045";
         public const string podzialSekcjiNaStronieNieparzystej1 = @"\sectd \ltrsect\sbkodd\linex0\headery708\footery708\colsx708\endnhere\sectlinegrid360\sectdefaultcl\sectrsid13698412\sftnbj \pard\plain \ltrpar\ql \li0\ri0\sa200\sl276\slmult1\widctlpar\wrapdefault\aspalpha\aspnum\faauto\adjustright\rin0\lin0\itap0 \rtlch\fcs1 \af31507\afs22\alang1025 \ltrch\fcs0 \f31506\fs22\lang1045\langfe1033\cgrid\langnp1045\langfenp1033";
